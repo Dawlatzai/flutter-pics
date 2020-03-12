0d0ec5bd7d40bf124f74ab22a6953781b1c42191
Dawlatzai <dawlatzai.ghousi@gmail.com>
2020-03-12 18:41:02 +0430

    Add pics flutter project

---
+++ lib/src/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+
+}
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          print("Hello");
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Lets see the images!'),
+      ),
+    )
+  );
+  }
+}
