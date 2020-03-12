c27015f735957eec68c6328f6bdce2f1c16138be
Dawlatzai <dawlatzai.ghousi@gmail.com>
2020-03-12 18:54:26 +0430

    Add counter and use body widget for displaying the counter instance

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,14 +8,19 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
+
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
     home: Scaffold(
+      body: Text('$counter'),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: (){
-          print("Hello");
+          setState(() {
+            counter += 1;
+          });
         },
       ),
       appBar: AppBar(
